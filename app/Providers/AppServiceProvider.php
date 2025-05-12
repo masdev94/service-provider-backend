d17ff91f83a57b2fabc09b1328921c52118355d2
masdev94 <highlowww444@gmail.com>
2025-05-12 14:27:37 +0200

    fixed error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (App::environment('production')) {
+        if ($_ENV['APP_ENV'] === 'production') {
             URL::forceScheme('https');
         }
     }
