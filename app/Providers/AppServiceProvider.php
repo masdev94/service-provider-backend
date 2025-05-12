870988333f6d3e17441c25bcd65d5fa53d8bddce
masdev94 <highlowww444@gmail.com>
2025-05-12 14:40:10 +0200

    fixed deploy error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($_ENV['APP_ENV'] === 'production') {
+        if (App::environment('production')) {
             URL::forceScheme('https');
         }
     }
